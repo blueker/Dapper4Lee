17c0bfe7592bc55b8b0cd783ad691e5e61845f4f
blueker_li <492831856@qq.com>
2015-05-24 11:19:00 +0800

    first change

---
+++ DapperHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dapper4Lee
+{
+    public class DapperHelper
+    {
+        private static object LOCKOBJ = new object(); 
+        private DapperHelper()
+        { } 
+        private static DapperHelper _Instance;
+        public static DapperHelper Instance
+        {
+            get
+            {
+                if(_Instance==null)
+                {
+                    lock (LOCKOBJ)
+                    {
+                        if (_Instance == null)
+                            _Instance = new DapperHelper();
+                    }
+                }
+                return _Instance;
+            }
+        }
+    }
+}
